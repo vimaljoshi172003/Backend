d6ca02036b19dc127417e2308f5d344216dfcd44
vimal joshi <111500483+vimaljoshi172003@users.noreply.github.com>
2024-07-31 17:55:51 +0530

    Ecommerce Model

---
+++ Models/Ecommerce Model/order.models.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+    productID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true,
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    orderItem: {
+        type: [orderItemSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PENDING", "CANCELLED", "DELIVERED"],
+        default: "PENDING"
+    }
+}, { timestamps: true })
+
+export const Order = mongoose.model("Order", orderSchema)
---
+++ Models/Ecommerce Model/user.models.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    Email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema)
